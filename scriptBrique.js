9f8347f49e5807ba5d3da1018f0acb3208fcd628
Kellyan01 <depriester.yoann@neuf.fr>
2021-04-15 22:24:33 +0200

    Position Brique + Brique Aléatoire
    + Réglage du position des Briques par création d'une zone de Brique
    + Fonction de création de Brique à une position aléatoire
    + Appel de la fonction via un setInterval

--- scriptBrique.js
+++ scriptBrique.js
@@ -1,9 +1,21 @@
-function newBrique() {
-    let terrain = document.getElementById('terrain');
+function newBrique(top,left) {
+    let zoneBrique = document.getElementById('zoneBrique');
     let brique = document.createElement('div');
     
-    brique.classList.add ("terrain_brique");
-    brique.style.top =  "-80px";
-    //brique.style.left = left;
-    terrain.appendChild(brique);
+    brique.classList.add ("terrain_zoneBrique_brique");
+    brique.style.top =  top;
+    brique.style.left = left;
+    zoneBrique.appendChild(brique);
 }
+
+function newBrique2() {
+    let zoneBrique = document.getElementById('zoneBrique');
+    let brique = document.createElement('div');
+    
+    brique.classList.add ("terrain_zoneBrique_brique");
+    brique.style.top =  Math.round(Math.random()*7)*50 + "px";
+    brique.style.left = Math.round(Math.random()*15)*50 + "px";
+    zoneBrique.appendChild(brique);
+}
+
+window.setInterval(newBrique2, 1000);
